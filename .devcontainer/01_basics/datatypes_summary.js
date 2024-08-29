a2b7e9a4405a559c3e8a388161466dca3cae6cb4
SaurabhGite93 <saurabhgite68@gmail.com>
2024-08-29 14:08:50 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+//const Id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(Id === anotherId);
+
+
+const heros= ["shaktimaan", "naagraj", "doga"] ;
+const outsidetemp = null;
+let myOBJ = {
+ name: "Saurabh", 
+ age: 30,  
+}
+
+//const BigNumber = 123142321312354332n 
+
+const myFunction=function() 
+{
+    console.log("Hello World");
+    }
+
+    //console.log("function()" );
+    
+   console.log(typeof anotherId); 
+
+   
