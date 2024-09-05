168f2d0d3306e964ef6217c896f3d49d87635153
SaurabhGite93 <saurabhgite68@gmail.com>
2024-09-05 06:56:54 +0000

    //done and dusted

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Saurabh"
+const repocount = 50
+
+//onsole.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const gameName = new String('Saurabhtc')
+
+//console.log(gameName);
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+/*console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('a'));
+*/
+
+const newString = gameName.substring(0, 4);
+//console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4)
+
+//console.log(anotherString);
+
+
+const newStringOne= '  saurabh   '
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
